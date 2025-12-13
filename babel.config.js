03ffa9ee34acdd047abb2626708579acee69c593
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2025-12-13 22:00:56 +0700

    chore: remove Android and iOS project files and update configuration
    - Deleted Android and iOS project files, including build configurations, resources, and source code.
    - Updated .gitignore to exclude Android and iOS directories.
    - Adjusted babel.config.js to include new aliases and plugins.
    - Updated eas.json to reflect changes in build configurations for Android.
    - Added new dependencies in package.json and package-lock.json for improved functionality.

--- babel.config.js
+++ babel.config.js
@@ -1,37 +1,31 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: [
-            ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-            "nativewind/babel",
+      presets: [
+        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+        "nativewind/babel",
+      ],
+      plugins: [
+        [
+          "module-resolver",
+          {
+            root: ["./"],
+            alias: {
+              "@assets": "./assets",
+              "@components": "./src/components",
+              "@hooks": "./src/hooks",
+              "@models": "./src/models",
+              "@utils": "./src/utils",
+              "@stores": "./src/stores",
+              "@services": "./src/services",
+              "@configs": "./src/configs",
+              "@constants": "./src/constants",
+              "@routes": "./src/routes",
+            },
+            extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".lottie"],
+          },
         ],
-        plugins: [
-            [
-                "module-resolver",
-                {
-                    root: ["./"],
-                    alias: {
-                        "@assets": "./assets",
-                        "@components": "./src/components",
-                        "@hooks": "./src/hooks",
-                        "@models": "./src/models",
-                        "@utils": "./src/utils",
-                        "@stores": "./src/stores",
-                        "@services": "./src/services",
-                        "@configs": "./src/configs",
-                        "@constants": "./src/constants",
-                        "@routes": "./src/routes",
-                    },
-                    extensions: [
-                        ".js",
-                        ".jsx",
-                        ".ts",
-                        ".tsx",
-                        ".json",
-                        ".lottie",
-                    ],
-                },
-            ],
-        ],
+        "react-native-reanimated/plugin",
+      ],
     };
-};
+  };
